07006359c5c10b45f48faf1305850680b570cf4d
TioRuben <tioruben@gmail.com>
2019-09-07 21:18:39 +0200

    fix failing travis

--- include/settings.example.h
+++ include/settings.example.h
@@ -1,1 +1,7 @@
 #define OPENWEATHER_API_URL OPEN_WEATHER_MAP_URL = "http://api.openweathermap.org/data/2.5/weather?q=CITY_HERE&units=metric&APPID=nnnn"
+
+#define HOLD_TIMEOUT 2000
+#define DEBOUNCETIME 60
+
+#define WIFI_AP_NAME "iArradio"
+#define WIFI_AP_PASS "iArradio123"
